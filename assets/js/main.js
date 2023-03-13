5e9d562e42421e2119a8808a46608e2713e7f86b
lemuelyu95 <lemuelyu95@gmail.com>
2023-03-13 13:16:22 +0800

    portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*===== MENU SHOW =====*/ 
+// Show menu
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -11,16 +11,17 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-/*===== REMOVE MENU MOBILE =====*/
+// Remove menu mobile
 const navLink = document.querySelectorAll('.nav__link')
 
+// Get rid of menu after clicking 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+// Scroll sections active link
 const sections = document.querySelectorAll('section[id]')
 
 window.addEventListener('scroll', scrollActive)
@@ -50,23 +51,18 @@ const sr = ScrollReveal({
 
 sr.reveal('.home__title', {})
 sr.reveal('.home__scroll', {delay: 200})
-sr.reveal('.home__img', {origin:'right', delay: 400})
 
-/*SCROLL ABOUT*/
 sr.reveal('.about__img', {delay: 400})
 sr.reveal('.about__subtitle', {delay: 300})
 sr.reveal('.about__profession', {delay: 400})
 sr.reveal('.about__text', {delay: 400})
 sr.reveal('.about__social-icon', {delay: 400})
 
-/*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle', {})
 sr.reveal('.skills__name', {distance: '20px', delay: 25, interval: 50})
 sr.reveal('.skills__img', {delay: 400})
 
-/*SCROLL PORTFOLIO*/
 sr.reveal('.portfolio__img', {interval: 200})
 
-/*SCROLL CONTACT*/
 sr.reveal('.contact__subtitle', {})
 sr.reveal('.contact__text', {interval: 200})
